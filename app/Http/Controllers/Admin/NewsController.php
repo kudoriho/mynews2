60ee4e152cd09ebd4f9d16cd1f73f52d57110171
名前 <>
2022-03-16 18:03:18 +0900

    PHP/Laravel 13 – [Mac編]ユーザー認証を実装する

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    // 以下を追記
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+}
